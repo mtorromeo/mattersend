439233fe44c618384542423a13197786692ef754
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2016-02-05 15:09:08 +0100

    pep8 fix

--- mattersend.py
+++ mattersend.py
@@ -83,7 +83,7 @@ def build(options, args, message):
 
 
 def sizeof_fmt(num, suffix='B'):
-    for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+    for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']:
         if abs(num) < 1024.0:
             return "%3.1f%s%s" % (num, unit, suffix)
         num /= 1024.0
