b8cbd3bddd68531bbf73a5df219c9b3902123c5e
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2016-02-22 14:15:52 +0100

    Added dependency for nose coverage

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
         ],
     },
     install_requires=["setproctitle", "requests"],
-    tests_require=["nose", "pyfakefs"],
+    tests_require=["nose", "coverage", "pyfakefs"],
     test_suite="nose.collector",
     version=mattersend.version,
     description=mattersend.description,
